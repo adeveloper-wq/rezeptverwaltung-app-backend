9e2022958a4107a19d16b026b0ee83dd99b6325c
adeveloper-wq <dajesch@gmail.com>
2021-08-10 22:43:41 +0200

    status codes for success and fail and auth check added

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -7,53 +7,85 @@ use Illuminate\Http\Request;
 
 class GroupController extends Controller{
     public function index(){
+        $this->middleware('auth:api');
         $groups = Group::all();
-        return response()->json($groups);
+
+        if($groups){
+            return response()->json($groups, 200);
+        }else{
+            return response()->json("Can't get groups.", 500);
+        }
     }
 
     public function get($name){
+        $this->middleware('auth:api');
         $group = Group::find($name);
-        return response()->json($group);
+
+        if($group){
+            return response()->json($group, 200);
+        }else{
+            return response()->json("Can't get group.", 500);
+        }
     }
 
     public function create(Request $request){
+        $this->middleware('auth:api');
         $group = new Group();
 
         $group->passwort    = $request->passwort;
         $group->hinweis     = $request->hinweis;
         $group->admin_P_ID  = $request->admin_P_ID;
 
-        $group->save();
+        $check = $group->save();
 
-        return response()->json("Group Successfully Created!");
+        if($check){
+            return response()->json("Group Successfully Created!", 200);
+        }else{
+            return response()->json("Creating the group failed!", 500);
+        }
     }
 
     public function update(Request $request, $G_ID){
+        $this->middleware('auth:api');
         $group = Group::find($G_ID);
 
         $group->passwort    = $request->passwort;
         $group->hinweis     = $request->hinweis;
 
-        $group->save();
+        $check = $group->save();
 
-        return response()->json($group);
+        if($check){
+            return response()->json($group, 200);
+        }else{
+            return response()->json('Updating the group failed!', 500);
+        }
     }
 
     public function delete($G_ID){
+        $this->middleware('auth:api');
         $group = Group::find($G_ID);
-        $group->delete();
+        $check = $group->delete();
 
-        return response()->json("Group Successfully Deleted!");
+        if($check){
+            return response()->json('Group Successfully Deleted!', 200);
+        }else{
+            return response()->json('Deleting the group failed!', 500);
+        }
     }
 
     public function join(Request $request){
+        $this->middleware('auth:api');
         $membership = new Membership();
 
         $membership->P_ID    = $request->P_ID;
         $membership->G_ID    = $request->G_ID;
 
-        $membership->save();
+        $check = $membership->save();
 
-        return response()->json("Group Successfully Joined!");
+        if($check){
+            return response()->json('Group successfully joined!', 200);
+        }else{
+            return response()->json('Joining the group failed!', 500);
+        }
     }
 }
